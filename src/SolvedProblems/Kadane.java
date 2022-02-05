a5b3ac12d246d7c9e88a7a7d24f873516f65b470
shubham409 <sm091274@gmail.com>
2022-02-05 15:20:45 +0530

    making separate package for solved problems

---
+++ src/SolvedProblems/Kadane.java
@@ -0,0 +1,18 @@
+package SolvedProblems;
+
+class Kadane {
+    int maxSubarraySum(int arr[], int size) {
+        int max_ending_here = 0, max_so_far = Integer.MIN_VALUE;
+        for (int i = 0; i < size; i++) {
+
+            if (arr[i] <= max_ending_here + arr[i]) {
+                max_ending_here += arr[i];
+            } else {
+                max_ending_here = arr[i];
+            }
+            if (max_ending_here > max_so_far)
+                max_so_far = max_ending_here;
+        }
+        return max_so_far;
+    }
+}
